0b97b48a3430d79d356d830ebd65fb9f3be71d85
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-03-24 21:02:16 +0530

    custom text input field

--- ExpenseTracker/app/src/main/java/com/example/myapplication/View/Components/CustomTextInput.kt
+++ ExpenseTracker/app/src/main/java/com/example/myapplication/View/Components/CustomTextInput.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.View.Components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -12,10 +13,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,13 +27,15 @@ import androidx.compose.ui.unit.sp
 fun CustomTextInput(){
     var value by remember{mutableStateOf("")}
     TextField(value = value, onValueChange = {value = it},
-        placeholder = {Text("Enter Amount", fontSize =  25.sp, color = Color.Black)},
+        placeholder = {Text("Enter Amount", fontSize =  20.sp, color = Color.Black, fontWeight = FontWeight.Light)},
         modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
         textStyle = TextStyle(fontSize = 30.sp, color = Color.Black, fontWeight = FontWeight.SemiBold),
         maxLines = 1,
         colors = TextFieldDefaults.textFieldColors(
-            containerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Black
+            containerColor = colorResource(id = R.color.tertiary),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
         )
     )
 }
