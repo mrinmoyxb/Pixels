59895ab7f9e60400fe81f886c44c35b24c573973
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-03-28 20:41:44 +0530

    completed earning and expense screen

--- ExpenseTracker/app/src/main/java/com/example/myapplication/View/S3_DepositWithdrawScreen/AddEarningsScreen.kt
+++ ExpenseTracker/app/src/main/java/com/example/myapplication/View/S3_DepositWithdrawScreen/AddEarningsScreen.kt
@@ -1,18 +1,45 @@
 package com.example.myapplication.View.S3_DepositWithdrawScreen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
 import com.example.myapplication.View.Components.CustomButton
 import com.example.myapplication.View.Components.CustomDropDownMenu
@@ -20,8 +47,12 @@ import com.example.myapplication.View.Components.CustomTextInput
 import com.example.myapplication.View.Components.Item
 import com.example.myapplication.View.Components.TextHeading
 import com.example.myapplication.ViewModel.AddAmountViewModel.AddAmountViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEarningsScreen(viewModel: AddAmountViewModel){
 
@@ -35,6 +66,21 @@ fun AddEarningsScreen(viewModel: AddAmountViewModel){
         Item("Wages", R.drawable.wages),
         Item("Others", R.drawable.others),
     )
+
+    // for input boxes
+    var amount by remember{ mutableStateOf("") }
+    var description by remember{ mutableStateOf("") }
+
+    // for drop down box
+    var isExpanded by remember{ mutableStateOf(false) }
+    var earningType by remember{ mutableStateOf("Select an earning type") }
+    var iconToDisplay by remember { mutableIntStateOf(R.drawable.expenses) }
+
+    // date
+    val currentMillis = System.currentTimeMillis()
+    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val formattedDate = simpleDateFormat.format(remember { Calendar.getInstance().time })
+
     Column(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
@@ -50,22 +96,112 @@ fun AddEarningsScreen(viewModel: AddAmountViewModel){
             // Add amount
             TextHeading(title = "Amount", fontSize = 23, fontWeight = FontWeight.SemiBold, color = Color.Black)
             Spacer(modifier = Modifier.height(4.dp))
-            CustomTextInput("Enter amount")
+            TextField(value = amount, onValueChange = {amount = it},
+                placeholder = { Text("Enter the amount", fontSize =  20.sp, color = Color.Black, fontWeight = FontWeight.Light) },
+                modifier = Modifier.fillMaxWidth().height(70.dp),
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(fontSize = 25.sp, color = Color.Black, fontWeight = FontWeight.SemiBold),
+                maxLines = 1,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = colorResource(id = R.color.tertiary),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
             Spacer(modifier = Modifier.height(10.dp))
 
-            // Expense type(drop down menu):
+            // Earning type(drop down menu):
             TextHeading(title = "Earning type", fontSize = 23, fontWeight = FontWeight.SemiBold, color = Color.Black)
             Spacer(modifier = Modifier.height(4.dp))
-            CustomDropDownMenu(options, "Select an earning type")
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .fillMaxWidth()
+                    .height(70.dp)
+                    .background(
+                        color = colorResource(id = R.color.tertiary),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            ) {
+                ExposedDropdownMenuBox(expanded = isExpanded, onExpandedChange = { isExpanded = !isExpanded }) {
+
+                    Row(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 15.dp, end = 10.dp, top = 3.dp, bottom = 3.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            painter = painterResource(id = iconToDisplay),
+                            contentDescription = "",
+                            modifier = Modifier.size(30.dp)
+                        )
+                        TextField(
+                            value = earningType,
+                            onValueChange = {},
+                            modifier = Modifier
+                                .menuAnchor()
+                                .fillMaxSize(),
+                            readOnly = true,
+                            textStyle = TextStyle(fontSize = 23.sp, fontWeight = FontWeight.Light),
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = Color.Transparent,
+                                cursorColor = Color.Black,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            )
+                        )
+                    }
+
+                    ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                        options.forEach { item ->
+                            DropdownMenuItem(
+                                text = {
+                                    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
+                                        Icon(painter = painterResource(id = item.icon), contentDescription = "", modifier = Modifier.size(15.dp))
+                                        Spacer(modifier = Modifier.width(10.dp))
+                                        Text(item.name, fontWeight = FontWeight.SemiBold, fontSize = 20.sp)
+                                    }
+                                },
+
+                                onClick = {
+                                    earningType = item.name
+                                    iconToDisplay = item.icon
+                                    isExpanded = false
+                                }
+                            )
+                        }
+                    }
+
+                }
+            }
             Spacer(modifier = Modifier.height(10.dp))
 
             // Enter description:
             TextHeading(title = "Description", fontSize = 23, fontWeight = FontWeight.SemiBold, color = Color.Black)
             Spacer(modifier = Modifier.height(4.dp))
-            CustomTextInput("Write a short description...")
+            TextField(value = description, onValueChange = {description = it},
+                placeholder = {Text("Write a short description...", fontSize =  20.sp, color = Color.Black, fontWeight = FontWeight.Light)},
+                modifier = Modifier.fillMaxWidth().height(70.dp),
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(fontSize = 25.sp, color = Color.Black, fontWeight = FontWeight.SemiBold),
+                maxLines = 1,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = colorResource(id = R.color.tertiary),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
             Spacer(modifier = Modifier.height(30.dp))
-
-            CustomButton(text = "Done", fontSize = 25, boxColor = colorResource(id = R.color.primary), textColor = Color.White)
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .clickable { viewModel.addExpenseTransaction(amount = amount.toDouble(), type = earningType, description = description,
+                    formattedDate = formattedDate, dateAddedInMillis = currentMillis)}
+                .background(Color.Transparent),
+                shape = RoundedCornerShape(12.dp), colors = CardDefaults.cardColors(colorResource(id = R.color.primary))){
+                Box(modifier = Modifier.fillMaxSize().padding(10.dp), contentAlignment = Alignment.Center){
+                    Text("Add as earning", color = Color.White, fontSize = 25.sp, fontWeight = FontWeight.SemiBold)
+                }
+            }
         }
     }
 }
